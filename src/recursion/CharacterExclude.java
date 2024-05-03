b76e5633350ed27a502624b69995d947cb84c632
wownepal <phurba.sherpa@wowfinstacknepal.com>
2024-05-03 07:08:41 +0545

    Recursion: write a recursive solution to skip a word from the sentence

--- src/recursion/CharacterExclude.java
+++ src/recursion/CharacterExclude.java
@@ -3,12 +3,19 @@ package recursion;
 public class CharacterExclude {
     public static void main(String[] args) {
         String updatedString = getUpdatedString("Hello world", 0, 'o');
-        System.out.println(updatedString);
+        System.out.println(getUpdatedString("Hello, world !", ", world"));
     }
 
     static String getUpdatedString(String word, int currentIndex, char excludeChar) {
         if (word.length() == currentIndex) return "";
         char charAtCurrentIndex = word.charAt(currentIndex);
         return "" + (charAtCurrentIndex == excludeChar ? "" : charAtCurrentIndex) + getUpdatedString(word, currentIndex+1, excludeChar);
+    }
+
+    static String getUpdatedString(String words, String skipWord) {
+        if (words.isEmpty()) return "";
+        if (words.startsWith(skipWord)) return getUpdatedString(words.substring(skipWord.length()), skipWord);
+        return words.charAt(0) + getUpdatedString(words.substring(1), skipWord);
+
     }
 }
